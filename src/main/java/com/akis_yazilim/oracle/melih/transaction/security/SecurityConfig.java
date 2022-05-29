775285a19696139e4968e36cb982b71dfaad781a
MelihUslu <m.melihuslu@gmail.com>
2022-05-30 01:01:25 +0300

    initial commit

--- src/main/java/com/akis_yazilim/oracle/melih/transaction/security/SecurityConfig.java
+++ src/main/java/com/akis_yazilim/oracle/melih/transaction/security/SecurityConfig.java
@@ -1,6 +1,39 @@
 package com.akis_yazilim.oracle.melih.transaction.security;
 
-public class SecurityConfig
-{
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter
+{
+    @Value("${service.security.secure-key-username}")
+    private String secureKeyUserName;
+
+    @Value("${service.security.secure-key-password}")
+    private String secureKeyPassword;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .passwordEncoder(encoder)
+                .withUser(secureKeyUserName)
+                .password(encoder.encode(secureKeyPassword)).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception
+    {
+        super.configure(httpSecurity);
+
+        httpSecurity.csrf().disable();
+    }
 }
