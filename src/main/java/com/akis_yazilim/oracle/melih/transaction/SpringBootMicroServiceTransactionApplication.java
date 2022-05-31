6fe1ce9af34bb59b68eefa5a11ed4cfbb809752d
MelihUslu <m.melihuslu@gmail.com>
2022-06-01 00:41:19 +0300

    initial commit

--- src/main/java/com/akis_yazilim/oracle/melih/transaction/SpringBootMicroServiceTransactionApplication.java
+++ src/main/java/com/akis_yazilim/oracle/melih/transaction/SpringBootMicroServiceTransactionApplication.java
@@ -3,8 +3,10 @@ package com.akis_yazilim.oracle.melih.transaction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
 public class SpringBootMicroServiceTransactionApplication {
 
 	public static void main(String[] args) {
